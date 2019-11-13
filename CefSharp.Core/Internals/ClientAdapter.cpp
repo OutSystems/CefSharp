56eb03643e5c409a23ee566d65245b6c2eb51443
joaoneves <joaomiguelneves@gmail.com>
2019-11-13 12:17:20 +0000

    Add more information to crash in process message received

--- CefSharp.Core/Internals/ClientAdapter.cpp
+++ CefSharp.Core/Internals/ClientAdapter.cpp
@@ -1217,18 +1217,24 @@ namespace CefSharp
             else if (name == kJavascriptAsyncMethodCallRequest && !_browserAdapter->IsDisposed)
             {
                 auto frameId = frame->GetIdentifier();
-                auto objectId = GetInt64(argList, 0);
-                auto callbackId = GetInt64(argList, 1);
                 auto methodName = StringUtils::ToClr(argList->GetString(2));
-                auto arguments = argList->GetList(3);
-                auto methodInvocation = gcnew MethodInvocation(browser->GetIdentifier(), frameId, objectId, methodName, (callbackId > 0 ? Nullable<int64>(callbackId) : Nullable<int64>()));
-                for (auto i = 0; i < static_cast<int>(arguments->GetSize()); i++)
+                try
                 {
-                    methodInvocation->Parameters->Add(DeserializeObject(arguments, i, callbackFactory));
+                    auto objectId = GetInt64(argList, 0);
+                    auto callbackId = GetInt64(argList, 1);
+                    auto arguments = argList->GetList(3);
+                    auto methodInvocation = gcnew MethodInvocation(browser->GetIdentifier(), frameId, objectId, methodName, (callbackId > 0 ? Nullable<int64>(callbackId) : Nullable<int64>()));
+                    for (auto i = 0; i < static_cast<int>(arguments->GetSize()); i++)
+                    {
+                        methodInvocation->Parameters->Add(DeserializeObject(arguments, i, callbackFactory));
+                    }
+
+                    _browserAdapter->MethodRunnerQueue->Enqueue(methodInvocation);
                 }
-
-                _browserAdapter->MethodRunnerQueue->Enqueue(methodInvocation);
-
+                catch (Exception ^ e)
+                {
+                    throw gcnew Exception("Exception handling javascript async method call: " + methodName, e);
+                }
                 handled = true;
             }
             else if (name == kJavascriptMessageReceived)
